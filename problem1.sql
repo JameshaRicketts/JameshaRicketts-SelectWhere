ea15ba9a3aa49679ec48fe55b94a719230160f2b
JameshaRicketts <jamesharicketts@gmail.com>
2023-02-15 01:05:12 +0000

    done

---
+++ problem1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employee WHERE last_name = 'Smith';
+--#### Problem 1: Given the above employee table, query all the 
+--records from the table that have the last_name 'Smith'. Remember 
+--to use single quotes when filtering for varchars, not double quotes.
---
+++ problem2.sql
@@ -0,0 +1,3 @@
+SELECT * FROM employee WHERE salary > 75000;
+--### Problem 2: Query the employee table for all records with a 
+--salary greater than $75,000
